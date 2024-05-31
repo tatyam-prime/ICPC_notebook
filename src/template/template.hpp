510ecc53f180a09b0e96e073ddc94ca7415be9ac
tatyam <tatuhito0125@yahoo.co.jp>
2024-05-31 15:50:32 +0900

    chmin / chmax って短くなるよね

--- src/template/template.hpp
+++ src/template/template.hpp
@@ -5,16 +5,8 @@ const ll INF = LLONG_MAX / 4;
 #define rep(i, a, b) for(ll i = a; i < (b); i++)
 #define all(a) begin(a), end(a)
 #define sz(a) ssize(a)
-bool chmin(auto& a, auto b) {
-   if(a <= b) return 0;
-   a = b;
-   return 1;
-}
-bool chmax(auto& a, auto b) {
-   if(a >= b) return 0;
-   a = b;
-   return 1;
-}
+bool chmin(auto& a, auto b) { return a > b ? a = b, 1 : 0; }
+bool chmax(auto& a, auto b) { return a < b ? a = b, 1 : 0; }
 
 int main() {
    cin.tie(0)->sync_with_stdio(0);

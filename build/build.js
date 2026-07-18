b9064967b873b9a4878f9a13228bd568311ab448
tatyam <40888698+tatyam-prime@users.noreply.github.com>
2026-07-19 05:25:24 +0900

    bump requirements

--- build/build.js
+++ build/build.js
@@ -118,7 +118,7 @@ fs.writeFileSync("build/notebook.html", `<!DOCTYPE html>
             }
         };
     </script>
-    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
+    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@4.1.3/tex-mml-chtml.js"></script>
     <!-- css -->
     <link rel="stylesheet" type="text/css" href="notebook.css" />
 </head>
